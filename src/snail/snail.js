1a69c7b3d91417bf40f9eadf2a57cf4db363623e
Ed Ignot <edignot@gmail.com>
2020-10-21 09:58:24 -0600

    Snail comments cleanup

--- src/snail/snail.js
+++ src/snail/snail.js
@@ -1,18 +1,6 @@
 // Problem: SnailSort is an algorithm that takes an array of equal-length sub-arrays, and then merges them into a single array in a clockwise spiral, starting from the upper-left hand corner.
-// Time complexity: 
-const arrayMatrix = [
-    [1, 2, 3, 4, 5],
-    [6, 7, 8, 9, 10],
-    [11, 12, 13, 14, 15],
-    [16, 17, 18, 19, 20],
-    [21, 22, 23, 24, 25],
-];
+// Time complexity:
 
-// const arrayMatrix = [
-//   [1, 2, 3],
-//   [6, 7, 8],
-//   [11, 12, 13],
-// ];
 const snail = (arrayMatrix) => {
     const finalArray = [];
     while (arrayMatrix.length > 0) {
@@ -42,4 +30,4 @@ const snail = (arrayMatrix) => {
     return finalArray;
 };
 
-export default snail
+export default snail;
